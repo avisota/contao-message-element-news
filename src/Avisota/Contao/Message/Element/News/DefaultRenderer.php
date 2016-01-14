e2da6da89c1e50e766883ab13cd13dbb38f4c48a
Sven Baumann <baumann.sv@gmail.com>
2016-01-14 15:47:59 +0100

    edit author, copyright

--- src/Avisota/Contao/Message/Element/News/DefaultRenderer.php
+++ src/Avisota/Contao/Message/Element/News/DefaultRenderer.php
@@ -2,12 +2,12 @@
 
 /**
  * Avisota newsletter and mailing system
- * Copyright (C) 2013 Tristan Lins
+ * Copyright © 2016 Sven Baumann
  *
  * PHP version 5
  *
- * @copyright  bit3 UG 2013
- * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  way.vision 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @package    avisota/contao-message-element-article
  * @license    LGPL-3.0+
  * @filesource
@@ -26,7 +26,7 @@ use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
 use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\GetArticleEvent;
 use ContaoCommunityAlliance\Contao\Bindings\Events\News\GetNewsEvent;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;w
 
 /**
  * Class DefaultRenderer
