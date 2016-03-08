06bbc492031ddebdb69aa114939351ed378caf74
Sven Baumann <baumann.sv@gmail.com>
2016-03-08 11:17:08 +0100

    make successful find content element template by page or newsletter theme

--- src/DefaultRenderer.php
+++ src/DefaultRenderer.php
@@ -16,6 +16,7 @@
 namespace Avisota\Contao\Message\Element\News;
 
 use Avisota\Contao\Core\Message\Renderer;
+use Avisota\Contao\Entity\MessageContent;
 use Avisota\Contao\Message\Core\Event\AvisotaMessageEvents;
 use Avisota\Contao\Message\Core\Event\RenderMessageContentEvent;
 use Contao\Doctrine\ORM\Entity;
@@ -77,9 +78,10 @@ class DefaultRenderer implements EventSubscriberInterface
         /** @var EntityAccessor $entityAccessor */
         $entityAccessor = $container['doctrine.orm.entityAccessor'];
 
+        $contentEventTemplate = $this->findNewsTemplate($content);
         $getNewsEvent = new GetNewsEvent(
             $content->getNewsId(),
-            $content->getNewsTemplate()
+            $contentEventTemplate
         );
 
         /** @var EventDispatcher $eventDispatcher */
@@ -91,7 +93,83 @@ class DefaultRenderer implements EventSubscriberInterface
 
         $template = new \TwigTemplate('avisota/message/renderer/default/mce_news', 'html');
         $buffer   = $template->parse($context);
+        $this->removeMicroTimeTemplate($content, $contentEventTemplate);
 
         $event->setRenderedContent($buffer);
+    }
+
+    protected function findNewsTemplate(MessageContent $content)
+    {
+        $messageCategory = $content->getMessage()->getCategory();
+        $messageTheme    = $messageCategory->getLayout()->getTheme();
+
+        $template = null;
+        if ($messageTheme->getTemplateDirectory()
+            && file_exists(TL_ROOT . '/templates/' . $messageTheme->getTemplateDirectory() . '/' . $content->getNewsTemplate() . '.html5')
+        ) {
+            $template = $this->copyTemplateInRootTemplates(
+                $messageTheme->getTemplateDirectory() . '/' . $content->getNewsTemplate(),
+                '.' . microtime(true)
+            );
+        }
+        if (!$template
+            && $messageCategory->getViewOnlinePage() > 0
+        ) {
+            $viewOnlinePage = \PageModel::findByPk($messageCategory->getViewOnlinePage());
+
+            $pageTheme = null;
+            if ($viewOnlinePage) {
+                $viewOnlinePage->loadDetails();
+                $pageTheme = $viewOnlinePage->getRelated('layout')->getRelated('pid');
+            }
+
+            if ($pageTheme
+                && file_exists(TL_ROOT . '/' . $pageTheme->templates . '/' . $content->getNewsTemplate() . '.html5')
+            ) {
+                $source = $pageTheme->templates;
+                $chunks = explode('/', $source);
+                if (count($chunks) > 1) {
+                    if (in_array('templates', array_values($chunks))) {
+                        $unset = array_flip($chunks)['templates'];
+                        unset($chunks[$unset]);
+                    }
+                }
+                $source = implode('/', $chunks);
+
+                $template = $this->copyTemplateInRootTemplates(
+                    $source . '/' . $content->getNewsTemplate(),
+                    '.' . microtime(true)
+                );
+            }
+        }
+
+        if (!$template) {
+            $template = $content->getNewsTemplate();
+        }
+
+
+        return $template;
+    }
+
+    protected function copyTemplateInRootTemplates($source, $destination)
+    {
+        $sourceFile = new \File('templates/' . $source . '.html5');
+        $sourceFile->copyTo('templates/' . $destination . '.html5');
+
+        return $destination;
+    }
+
+    protected function removeMicroTimeTemplate(MessageContent $content, $remove)
+    {
+        if ($content->getNewsTemplate() === $remove) {
+            return;
+        }
+
+        $removeFile = new \File('templates/' . $remove . '.html5', true);
+        if (!$removeFile->exists()) {
+            return;
+        }
+
+        $removeFile->delete();
     }
 }
